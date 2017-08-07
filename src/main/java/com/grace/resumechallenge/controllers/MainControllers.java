87a910bfc5abae261eca713f0f44ae3c03c2e8e7
grace-luning-fu <grace.luning.fu@gmail.com>
2017-08-06 23:26:21 -0400

    update comments and a few additions

--- src/main/java/com/grace/resumechallenge/controllers/MainControllers.java
+++ src/main/java/com/grace/resumechallenge/controllers/MainControllers.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import javax.validation.Valid;
@@ -18,8 +19,10 @@ public class MainControllers {
     @Autowired
     ResumeRepo resumeRepo;
 
+
+    //Home Page
     @GetMapping("/")
-    public String defaultRequest(Model model){
+    public String defaultRequest(Model model) {
         String myMessage = "welcome to the Resume Application";
         model.addAttribute("message", myMessage);
         return "welcome";
@@ -27,43 +30,49 @@ public class MainControllers {
 
 
     @GetMapping("/addresume")
-    public String addBook(Model model) {
+    public String addResume(Model model) {
         model.addAttribute("newResume", new Resume());
         return "addresume";
     }
 
     @PostMapping("/addresume")
-    public String postBook(@Valid @ModelAttribute("newResume") Resume resume, BindingResult bindingResult)
-    {
+    public String postResume(@Valid @ModelAttribute("newResume") Resume resume, BindingResult bindingResult) {
 
-        if(bindingResult.hasErrors()){
+        //if there are errors in validation constrains, prompt the user to add the resume again.
+        if (bindingResult.hasErrors()) {
             return "addresume";
         }
 
-
+        //Initialize a variable to store the calculated employed days
         long workdays;
 
-        if(resume.getEndDate()==null){
-            workdays= calDateFromToday(resume.getStartDate());
+
+        // If the user leave endDate empty, which means the user is still working,
+        // get the day difference between Start date and now
+        if (resume.getEndDate() == null) {
+            workdays = calDateFromToday(resume.getStartDate());
         }
-        else{
-            workdays = caldatediff(resume.getStartDate(),resume.getEndDate());
+        //If user input a endDate, fine the date difference between Start Date and end Date
+        else {
+            workdays = caldatediff(resume.getStartDate(), resume.getEndDate());
         }
 
+        //pass the calculated employed/worked days to the variable "workdays" in the Resume class
         resume.setWorkdays(workdays);
 
+        //save the instance into database
         resumeRepo.save(resume);
 
-        System.out.println("1\n" + calDateFromToday(resume.getStartDate()));
+        // print out the calculated workdays and double check with the value shown in HTML later
+        System.out.print("1\n" + workdays);
+        System.out.print("2\n" + resume.getWorkdays());
 
-        System.out.print("2\n"+workdays);
-
-        System.out.print("3\n"+resume.getWorkdays());
-
+        //return result page after successful addition
         return "result";
 
     }
 
+    //List all resumes in the database
     @GetMapping("/showallresume")
     public String showAllResumes(Model model) {
 
@@ -73,24 +82,22 @@ public class MainControllers {
 
     }
 
-    public long caldatediff(LocalDate date1, LocalDate date2)
-    {
+
+    // The method to calculate date difference between two dates, used in postResume method
+    public long caldatediff(LocalDate date1, LocalDate date2) {
 
         long days = ChronoUnit.DAYS.between(date1, date2);
         return days;
 
     }
 
-    public long calDateFromToday(LocalDate date1)
-    {
+    //The method to calculate date difference between a date and now, used in postResume method
+    public long calDateFromToday(LocalDate date1) {
 
         LocalDate today = LocalDate.now();
-        long days = ChronoUnit.DAYS.between(date1,today);
+        long days = ChronoUnit.DAYS.between(date1, today);
         return days;
 
     }
-
-
-
 
 }
--- src/main/java/com/grace/resumechallenge/models/Resume.java
+++ src/main/java/com/grace/resumechallenge/models/Resume.java
@@ -1,35 +1,57 @@
 package com.grace.resumechallenge.models;
 
+import org.hibernate.validator.constraints.Email;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.Date;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
 public class Resume {
 
+
+    //Should not have name as unique ID/primary key, because two persons may have the same name,
+    //or the same person have worked at two organization/more than one work experience
+    //if have name as @Id, the second entry of the same name will cover the first entry
     @NotNull
-    @Id
+    @Size(min=2)
     private String name;
 
+    //Since name and all other attributes are not fit to be the unique identifier, UUID is created as another attribute
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer uuid;
+
+    @Email
     private String email;
 
+    //no constrains required, since one can work freelance
     private String organization;
 
 
     @NotNull
     @DateTimeFormat(pattern = "yyyy/MM/dd")
+    //@Past doesn't work for LocalDate type, if user input a future date, Calculated WorkedDays will be negative
     private LocalDate startDate;
 
     @DateTimeFormat(pattern = "yyyy/MM/dd")
+    //@Past doesn't work for LocalDate data type. Works for data type--"date" though
     private LocalDate endDate;
 
-
+    //This is a calculated value, no constrain define here for now
     private long workdays;
 
+
+    public Integer getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(Integer uuid) {
+        this.uuid = uuid;
+    }
 
     public String getName() {
         return name;
@@ -79,4 +101,5 @@ public class Resume {
     public void setWorkdays(long workdays) {
         this.workdays = workdays;
     }
+
 }
